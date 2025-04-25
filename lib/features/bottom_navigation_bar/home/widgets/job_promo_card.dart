792157e633a931edb7ccc173175d4db95008d0e6
AyaEltantawy <121199487+AyaEltantawy@users.noreply.github.com>
2025-04-25 17:34:28 +0000

    first commit

--- lib/features/bottom_navigation_bar/home/widgets/job_promo_card.dart
+++ lib/features/bottom_navigation_bar/home/widgets/job_promo_card.dart
@@ -14,74 +14,82 @@ class JobPromoCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         color: Colors.amber[600],
       ),
-      child: Row(
+      child: Stack(
         children: [
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "See how you Can Find a job Easily",
-                    style: TextStyles.font16White600Weight
-
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 24,
                   ),
-                  const SizedBox(height: 10),
-                  Flexible(
-                    child: ElevatedButton(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "See how you Can Find a job Easily",
+                        style: TextStyles.font16White600Weight,
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
 
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
+              Expanded(
+                flex: 2,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/blue_rect.png",
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+
+                        child: Image.asset(
+                          "assets/images/woman.png",
+                          alignment: Alignment.center,
+                          height: 160,
+                          fit: BoxFit.contain,
                         ),
                       ),
-                      child: Text(
-                        textAlign: TextAlign.center,
-                        "More",
-                        style: TextStyles.font12SecondBlue600Weight,
-                      ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-
-
-          Expanded(
-            flex: 2,
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(20),
-                bottomRight: Radius.circular(20),
+          Positioned(
+            left: 50,
+            top: 120,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
               ),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                   Image.asset(
-                   "assets/images/blue_rect.png",
-                    width: double.infinity,
-                    height: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-
-                    child: Image.asset(
-                      "assets/images/woman.png",
-                      alignment: Alignment.center,
-                      height: 160,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  textAlign: TextAlign.center,
+                  "Read More",
+                  style: TextStyles.font12SecondBlue600Weight,
+                ),
               ),
             ),
           ),
